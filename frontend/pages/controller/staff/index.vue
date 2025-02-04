9e6295f23e048acb5fe23fa4ecdfc8dc7c9fc081
Teetat Teerawaropas <fame.tee@gmail.com>
2025-02-04 14:22:08 +0700

    [Admin] Staff Management Page

--- frontend/pages/controller/staff/index.vue
+++ frontend/pages/controller/staff/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="layout">
         <AdminSidebar />
-        <div class="main-content">
+        <div class="content">
             <div class="header">
                 <div class="title">Staff</div>
             </div>
@@ -13,6 +13,38 @@
 import AdminSidebar from '~/components/admin/AdminSidebar.vue';
 
 definePageMeta({
-    layout: false
+    layout: false,
+    middleware: 'admin-auth',
 })
 </script>
+
+<style scoped>
+.layout {
+    display: flex;
+    min-height: 100vh;
+    background: #ECECEC;
+    width: 100vw;
+}
+
+.content {
+    display: flex;
+    flex-direction: column;
+    padding: 20px;
+    flex: 1;
+}
+
+.title {
+    font-size: 3vw;
+}
+
+.sidebar-collapsed+.content {
+    margin-left: 80px;
+}
+
+.header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px 20px;
+}
+</style>
