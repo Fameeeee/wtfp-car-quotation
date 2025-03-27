edbfc05534a8b9ba6810c03e8816bcbc15f86898
Teetat-Teerawaropas <fame.tee@gmail.com>
2025-03-27 18:48:59 +0700

    [Admin] Change layout

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,9 +1,18 @@
 <template>
   <div>
-    <UApp>
       <NuxtLayout>
         <NuxtPage />
       </NuxtLayout>
-    </UApp>
   </div>
 </template>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  font-family: sans-serif;
+  font-style: normal;
+  font-weight: 500;
+}
+</style>
