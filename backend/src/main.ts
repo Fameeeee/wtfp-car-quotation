d9d1266f77b444916a564f4edbf05877675552a5
Teetat-Teerawaropas <fame.tee@gmail.com>
2025-08-18 13:20:49 +0700

    [Backend] Debug CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,8 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:3000',
-      'https://wtfp-car-quotation-gjlh0vtab-fameeeees-projects.vercel.app'
+      'https://wtfp-car-quotation-gjlh0vtab-fameeeees-projects.vercel.app',
+      'https://wtfp-car-quotation.vercel.app'
     ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Authorization',
