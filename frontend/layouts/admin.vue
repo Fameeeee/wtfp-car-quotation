27bbd9df1329ca544159079ee2a28f749a888ddd
Teetat-Teerawaropas <fame.tee@gmail.com>
2025-08-25 13:38:49 +0700

    [Admin] Create a admin layout

---
+++ frontend/layouts/admin.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="flex min-h-screen bg-[#ececec]">
+    <AdminSidebar :isOpen="isSidebarOpen" @toggle="isSidebarOpen = !isSidebarOpen" />
+
+    <div
+      class="transition-all flex-1"
+      :style="{ marginLeft: isSidebarOpen ? '280px' : '80px' }"
+    >
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import AdminSidebar from '~/components/admin/AdminSidebar.vue'
+import { ref } from 'vue'
+
+const isSidebarOpen = ref(false)
+</script>
