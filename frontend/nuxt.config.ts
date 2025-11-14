e3807885837f086303fad81b9a841a7ebf9f5905
Fameeeee <fame.tee@gmail.com>
2025-11-14 18:33:47 +0700

    Containerization

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -8,6 +8,9 @@ export default defineNuxtConfig({
   ],
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
+  devServer: {
+    port: 3000
+  },
   app: {
     head: {
       title: "Isuzu | Quotation",
